713109081a32e18d17e686a5227810c36428b57c
slacktracer <slacktracer@gmail.com>
2022-08-13 17:27:20 -0300

    Removing in-memory database paths/options from the persistence file. (Everything related to in-memory persistence in on the test project only now, as it should.)

--- src/persistence/persistence.ts
+++ src/persistence/persistence.ts
@@ -1,7 +1,5 @@
-import { DataType, newDb } from "pg-mem";
 import initPGPromise from "pg-promise";
 import { createClient } from "redis";
-import { v4 as uuid } from "uuid";
 
 import { transformColumnNames } from "./transform-column-names.js";
 
@@ -9,34 +7,15 @@ const pgp = initPGPromise();
 
 const options = process.env.LOCAL === "yes" ? { ssl: true } : {};
 
-export let pgm;
-
-if (process.env.USE_MEMORY_DATABASE === "yes") {
-  pgm = await newDb();
-
-  pgm.public.registerFunction({
-    args: [],
-    implementation: () => uuid(),
-    name: "gen_random_uuid",
-    returns: DataType.uuid,
-  });
-}
-
-export const db =
-  process.env.USE_MEMORY_DATABASE === "yes"
-    ? pgm.adapters.createPgPromise()
-    : pgp(options);
+export const db = pgp(options);
 
 db.$config.options.receive = transformColumnNames;
 
 export { loadQuery } from "./load-query.js";
 
-export const redisClient =
-  process.env.USE_MEMORY_DATABASE !== "yes"
-    ? createClient({
-        url: process.env.REDIS_CONNECTION_STRING,
-      })
-    : { connect: () => Promise.resolve(), quit: () => void 0 };
+export const redisClient = createClient({
+  url: process.env.REDIS_CONNECTION_STRING,
+});
 
 await redisClient.connect().catch(console.error);
 
