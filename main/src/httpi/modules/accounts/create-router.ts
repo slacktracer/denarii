f9a655a54724646c4cffffd306c61d9dc52c6f86
slacktracer <slacktracer@gmail.com>
2022-08-26 16:49:00 -0300

    New endpoints for Accounts.

--- main/src/httpi/modules/accounts/create-router.ts
+++ main/src/httpi/modules/accounts/create-router.ts
@@ -1,8 +1,10 @@
 import express from "express";
 
 import {
+  createAccount,
   readAccount,
   readAccounts,
+  updateAccount,
 } from "../../../core/modules/accounts/accounts.js";
 
 export const createRouter = () => {
@@ -24,6 +26,32 @@ export const createRouter = () => {
     const account = await readAccount({ accountID, userID });
 
     response.json(account);
+  });
+
+  accountsRouter.post("/", async (request, response) => {
+    const { userID } = request.session.user;
+
+    const { initialAmount, name } = request.body;
+
+    const data = { initialAmount, name };
+
+    const createdAccount = await createAccount({ data, userID });
+
+    response.json(createdAccount);
+  });
+
+  accountsRouter.post("/:accountID", async (request, response) => {
+    const { userID } = request.session.user;
+
+    const { accountID } = request.params;
+
+    const { initialAmount, name } = request.body;
+
+    const data = { initialAmount, name };
+
+    const updatedAccount = await updateAccount({ accountID, data, userID });
+
+    response.json(updatedAccount);
   });
 
   return accountsRouter;
