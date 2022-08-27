60717f5e9bb747d4ced1c54ee0998a2324bc57f8
slacktracer <slacktracer@gmail.com>
2022-08-27 12:46:52 -0300

    Moving the PATCH test to a patch.test files.

--- tests/__tests__/system/accounts.patch.test.js
+++ tests/__tests__/system/accounts.patch.test.js
@@ -8,6 +8,7 @@ import {
   test,
 } from "@jest/globals";
 
+import { accountID01 } from "../../data/data.js";
 import { endConnections } from "../../functions/end-connections.js";
 import { getServer } from "../../functions/get-server.js";
 import { getSessionIDCookie } from "../../functions/get-session-id-cookie.js";
@@ -44,30 +45,32 @@ beforeEach(async () => {
   backup.restore();
 });
 
-describe("/POST accounts", () => {
-  test("a new account is created and returned", async () => {
-    // given
-    const server = await getServer();
+describe("/PATCH accounts", () => {
+  describe("an account ID was given", () => {
+    test("an existing account is updated", async () => {
+      // given
+      const server = await getServer();
 
-    const initialAmount = 0;
-    const name = "New Account!";
+      const initialAmount = 0;
+      const name = "New Account!";
 
-    const expectedAccount = expect.objectContaining({ initialAmount, name });
+      const expectedAccount = expect.objectContaining({ initialAmount, name });
 
-    const sessionIDCookie = await getSessionIDCookie({
-      password: "1234",
-      server,
-      username: "mr.user",
+      const sessionIDCookie = await getSessionIDCookie({
+        password: "1234",
+        server,
+        username: "mr.user",
+      });
+
+      // when
+      const response = await server
+        .patch(`/accounts/${accountID01}`)
+        .send({ initialAmount, name })
+        .set("cookie", sessionIDCookie);
+
+      // then
+      expect(response.status).toEqual(200);
+      expect(response.body).toEqual(expectedAccount);
     });
-
-    // when
-    const response = await server
-      .post("/accounts")
-      .send({ initialAmount, name })
-      .set("cookie", sessionIDCookie);
-
-    // then
-    expect(response.status).toEqual(200);
-    expect(response.body).toEqual(expectedAccount);
   });
 });
