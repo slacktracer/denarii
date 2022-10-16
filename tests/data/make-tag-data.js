da2c0cb96f44e6becaadd448239d3c0351fbe78d
slacktracer <slacktracer@gmail.com>
2022-10-16 19:51:07 -0300

    Removing unnecessary pre-generated UUIDs for tags. (as was done for the other entities)

--- tests/data/make-tag-data.js
+++ tests/data/make-tag-data.js
@@ -1,115 +1,101 @@
 import { v4 as uuid } from "uuid";
 
 export const makeTagData = ({ operationID01, operationID03, userID01 }) => {
-  const tagID01 = uuid();
-  const tagID02 = uuid();
-  const tagID03 = uuid();
-  const tagID04 = uuid();
-  const tagID05 = uuid();
-  const tagKeyID01 = uuid();
-  const tagKeyID02 = uuid();
-  const tagKeyID03 = uuid();
-  const tagValueID01 = uuid();
-  const tagValueID02 = uuid();
-  const tagValueID03 = uuid();
-  const tagValueID04 = uuid();
-  const tagValueID05 = uuid();
-
-  const tag01 = {
-    createdAt: new Date().toISOString(),
-    tagID: tagID01,
-    tagKeyID: tagKeyID01,
-    tagValueID: tagValueID01,
-    operationID: operationID01,
-    userID: userID01,
-  };
-  const tag02 = {
-    createdAt: new Date().toISOString(),
-    tagID: tagID02,
-    tagKeyID: tagKeyID02,
-    tagValueID: tagValueID02,
-    operationID: operationID01,
-    userID: userID01,
-  };
-  const tag03 = {
-    createdAt: new Date().toISOString(),
-    tagID: tagID03,
-    tagKeyID: tagKeyID01,
-    tagValueID: tagValueID03,
-    operationID: operationID03,
-    userID: userID01,
-  };
-  const tag04 = {
-    createdAt: new Date().toISOString(),
-    tagID: tagID04,
-    tagKeyID: tagKeyID02,
-    tagValueID: tagValueID04,
-    operationID: operationID03,
-    userID: userID01,
-  };
-
-  const tag05 = {
-    createdAt: new Date().toISOString(),
-    tagID: tagID05,
-    tagKeyID: tagKeyID02,
-    tagValueID: tagValueID04,
-    operationID: operationID03,
-    userID: userID01,
-  };
-
   const tagKey01 = {
     createdAt: new Date().toISOString(),
     name: "Group",
-    tagKeyID: tagKeyID01,
+    tagKeyID: uuid(),
     userID: userID01,
   };
 
   const tagKey02 = {
     createdAt: new Date().toISOString(),
     name: "Category",
-    tagKeyID: tagKeyID02,
+    tagKeyID: uuid(),
     userID: userID01,
   };
 
   const tagKey03 = {
     createdAt: new Date().toISOString(),
     name: "Location",
-    tagKeyID: tagKeyID03,
+    tagKeyID: uuid(),
     userID: userID01,
   };
 
   const tagValue01 = {
     createdAt: new Date().toISOString(),
     name: "Some value 1",
-    tagValueID: tagValueID01,
+    tagValueID: uuid(),
     userID: userID01,
   };
 
   const tagValue02 = {
     createdAt: new Date().toISOString(),
     name: "Some value 2",
-    tagValueID: tagValueID02,
+    tagValueID: uuid(),
     userID: userID01,
   };
 
   const tagValue03 = {
     createdAt: new Date().toISOString(),
     name: "Some value 3",
-    tagValueID: tagValueID03,
+    tagValueID: uuid(),
     userID: userID01,
   };
 
   const tagValue04 = {
     createdAt: new Date().toISOString(),
     name: "Some value 4",
-    tagValueID: tagValueID04,
+    tagValueID: uuid(),
     userID: userID01,
   };
 
   const tagValue05 = {
     createdAt: new Date().toISOString(),
     name: "Some value 5",
-    tagValueID: tagValueID05,
+    tagValueID: uuid(),
+    userID: userID01,
+  };
+
+  const tag01 = {
+    createdAt: new Date().toISOString(),
+    tagID: uuid(),
+    tagKeyID: tagKey01.tagKeyID,
+    tagValueID: tagValue01.tagValueID,
+    operationID: operationID01,
+    userID: userID01,
+  };
+  const tag02 = {
+    createdAt: new Date().toISOString(),
+    tagID: uuid(),
+    tagKeyID: tagKey02.tagKeyID,
+    tagValueID: tagValue02.tagValueID,
+    operationID: operationID01,
+    userID: userID01,
+  };
+  const tag03 = {
+    createdAt: new Date().toISOString(),
+    tagID: uuid(),
+    tagKeyID: tagKey01.tagKeyID,
+    tagValueID: tagValue03.tagValueID,
+    operationID: operationID03,
+    userID: userID01,
+  };
+  const tag04 = {
+    createdAt: new Date().toISOString(),
+    tagID: uuid(),
+    tagKeyID: tagKey02.tagKeyID,
+    tagValueID: tagValue04.tagValueID,
+    operationID: operationID03,
+    userID: userID01,
+  };
+
+  const tag05 = {
+    createdAt: new Date().toISOString(),
+    tagID: uuid(),
+    tagKeyID: tagKey02.tagKeyID,
+    tagValueID: tagValue04.tagValueID,
+    operationID: operationID03,
     userID: userID01,
   };
 
