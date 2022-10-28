2349de79a53de91e60c5de60e95a13e8b2d945d8
slacktracer <slacktracer@gmail.com>
2022-10-28 16:11:41 -0300

    Removing traces of copy & paste. =P

--- main/src/httpi/modules/tags/route-handlers/post-tag-key.ts
+++ main/src/httpi/modules/tags/route-handlers/post-tag-key.ts
@@ -5,9 +5,9 @@ const { createTagKey } = tags;
 export const postTagKey = async (request, response) => {
   const { userID } = request.session.user;
 
-  const { initialAmount, name } = request.body;
+  const { name } = request.body;
 
-  const data = { initialAmount, name };
+  const data = { name };
 
   const createdTagKey = await createTagKey({ data, userID });
 
--- main/src/httpi/modules/tags/route-handlers/post-tag-value.ts
+++ main/src/httpi/modules/tags/route-handlers/post-tag-value.ts
@@ -5,9 +5,9 @@ const { createTagValue } = tags;
 export const postTagValue = async (request, response) => {
   const { userID } = request.session.user;
 
-  const { initialAmount, name } = request.body;
+  const { name } = request.body;
 
-  const data = { initialAmount, name };
+  const data = { name };
 
   const createdTagValue = await createTagValue({ data, userID });
 
