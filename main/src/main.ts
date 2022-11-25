9aeea4b70b62a173f3777b53147f045963a07df7
slacktracer <slacktracer@gmail.com>
2022-11-25 18:31:09 -0300

    The start of some sort of global error handling...

--- main/src/main.ts
+++ main/src/main.ts
@@ -1,1 +1,9 @@
 import "./httpi/httpi.js";
+
+process.on("uncaughtException", (...params) => {
+  console.error(params);
+});
+
+process.on("unhandledRejection", (...params) => {
+  console.error(params);
+});
