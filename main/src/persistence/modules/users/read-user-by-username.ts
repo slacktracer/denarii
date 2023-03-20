f7f6dde4db069177663ee0d7e62bc66bffe424ef
slacktracer <slacktracer@gmail.com>
2023-03-20 19:30:09 -0300

    Trying to track why Prisma fail running on Render.
    Works locally pointing to remote database just fine.

--- main/src/persistence/modules/users/read-user-by-username.ts
+++ main/src/persistence/modules/users/read-user-by-username.ts
@@ -1,7 +1,18 @@
 import { db } from "../../connect.js";
 
 export const readUserByUsername = async ({ username }) => {
-  const user = await db.user.findUnique({ where: { username } });
+  console.log("username", username);
 
-  return user;
+  try {
+    const user = await db.user.findUnique({ where: { username } });
+
+    return user;
+  } catch (error) {
+    console.log(error);
+
+    const user = await db.user.findFirst({ where: { username } });
+    console.log("user again", user);
+
+    return {};
+  }
 };
