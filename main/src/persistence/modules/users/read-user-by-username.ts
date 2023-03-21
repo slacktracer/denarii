d9e8726df32f63a2d007900ca50e93f3e046ce6f
slacktracer <slacktracer@gmail.com>
2023-03-20 21:46:51 -0300

    Revert "Trying to track why Prisma fail running on Render."
    This reverts commit f7f6dde4db069177663ee0d7e62bc66bffe424ef.

--- main/src/persistence/modules/users/read-user-by-username.ts
+++ main/src/persistence/modules/users/read-user-by-username.ts
@@ -1,18 +1,7 @@
 import { db } from "../../connect.js";
 
 export const readUserByUsername = async ({ username }) => {
-  console.log("username", username);
+  const user = await db.user.findUnique({ where: { username } });
 
-  try {
-    const user = await db.user.findUnique({ where: { username } });
-
-    return user;
-  } catch (error) {
-    console.log(error);
-
-    const user = await db.user.findFirst({ where: { username } });
-    console.log("user again", user);
-
-    return {};
-  }
+  return user;
 };
