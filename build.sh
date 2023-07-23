ebbf8750e8aea53a0fc06ca4d3ba4fe38294f72e
slacktracer <slacktracer@gmail.com>
2023-07-23 18:28:35 -0300

    Giving up on NPM workspaces. For now.
    Having individual package-locks helps making deploying simpler.

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ cp -ar ./main/src/. ./build && \
 cp -ar ./main/prisma/. ./build/prisma && \
 cp ./main/package.json ./build && \
 cp ./main/tsconfig.json ./build && \
-cp ./package-lock.json ./build && \
+cp ./main/package-lock.json ./build && \
 cd ./build && \
 npm ci --omit=dev && \
 ./node_modules/.bin/tsc --project ./tsconfig.json && \
