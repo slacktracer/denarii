ac1f63ddbe8fb50c9d69063be2369c9b9189edd7
slacktracer <slacktracer@gmail.com>
2023-09-29 15:56:21 -0300

    Minor improvements to showing test data.

--- tests/data/show-data.js
+++ tests/data/show-data.js
@@ -9,24 +9,30 @@ import {
 
 delete users.$;
 
+console.info("Users");
 console.table(users);
 
 delete accounts.$;
 
+console.info("Accounts");
 console.table(accounts);
 
 delete categories.$;
 
+console.info("Categories");
 console.table(categories);
 
 delete groups.$;
 
+console.info("Groups");
 console.table(groups);
 
 delete operations.$;
 
+console.info("Operations");
 console.table(operations);
 
 delete transfers.$;
 
+console.info("Transfers");
 console.table(transfers);
