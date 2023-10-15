cd4c39cdc252b53c68a444c7dda441d9ec3a4534
slacktracer <slacktracer@gmail.com>
2023-10-14 21:15:03 -0300

    More and much better tests for getting operations in datetime range.

--- tests/__tests__/integration/operations.get.test.js
+++ tests/__tests__/integration/operations.get.test.js
@@ -13,10 +13,17 @@ import * as mockConnect from "../../mocks/persistence/connect.js";
 
 vi.mock(`../../../main/src/persistence/connect.js`, () => mockConnect);
 
-const { user01 } = users.byBindingName;
-const { user01Password } = userPasswords.byBindingName;
+const { user01, user02 } = users.byBindingName;
+const { user01Password, user02Password } = userPasswords.byBindingName;
 
-const { operation01, operation05, operation11 } = operations.byBindingName;
+const {
+  operation01,
+  operation05,
+  operation10,
+  operation12,
+  operation13,
+  operation14,
+} = operations.byBindingName;
 
 describe("GET /operations", () => {
   test("it returns all the given user operations", async () => {
@@ -65,7 +72,7 @@ describe("GET /operations", () => {
       // given
       const server = await getServer();
 
-      const expectedOperations = [operation11]
+      const expectedOperations = [operation12]
         .map((operation) => Object.assign({}, operation))
         .map((operation) => {
           operation.updatedAt = null;
@@ -83,18 +90,102 @@ describe("GET /operations", () => {
           return operation;
         });
 
-      const from = "2023-10-03";
-      const to = "2023-10-04";
+      const from = "2023-08-01T00:00:00.000Z";
+      const to = "2023-08-07T21:00:00.000Z";
 
       const { secretCookie, sessionIDCookie } = await getSessionCookies({
-        password: user01Password,
+        password: user02Password,
         server,
-        username: user01.username,
+        username: user02.username,
       });
 
       // when
       const response = await server
         .get(`/operations?from=${from}&to=${to}`)
+        .set("cookie", [secretCookie, sessionIDCookie]);
+
+      // then
+      expect(response.status).toEqual(200);
+      expect(response.body).toEqual(expectedOperations);
+    });
+  });
+
+  describe("only a starting datetime is given", () => {
+    test("it returns all the given user operations since the given starting datetime", async () => {
+      // given
+      const server = await getServer();
+
+      const expectedOperations = [operation05, operation10, operation13]
+        .map((operation) => Object.assign({}, operation))
+        .map((operation) => {
+          operation.updatedAt = null;
+
+          operation.account = {
+            accountID: accounts.byID[operation.accountID].accountID,
+            name: accounts.byID[operation.accountID].name,
+          };
+
+          operation.category = {
+            categoryID: categories.byID[operation.categoryID].categoryID,
+            name: categories.byID[operation.categoryID].name,
+          };
+
+          return operation;
+        });
+
+      const from = "2023-08-07T21:01:00.000Z";
+
+      const { secretCookie, sessionIDCookie } = await getSessionCookies({
+        password: user02Password,
+        server,
+        username: user02.username,
+      });
+
+      // when
+      const response = await server
+        .get(`/operations?from=${from}`)
+        .set("cookie", [secretCookie, sessionIDCookie]);
+
+      // then
+      expect(response.status).toEqual(200);
+      expect(response.body).toEqual(expectedOperations);
+    });
+  });
+
+  describe("only a ending datetime is given", () => {
+    test("it returns all the given user operations from before the given ending datetime", async () => {
+      // given
+      const server = await getServer();
+
+      const expectedOperations = [operation14]
+        .map((operation) => Object.assign({}, operation))
+        .map((operation) => {
+          operation.updatedAt = null;
+
+          operation.account = {
+            accountID: accounts.byID[operation.accountID].accountID,
+            name: accounts.byID[operation.accountID].name,
+          };
+
+          operation.category = {
+            categoryID: categories.byID[operation.categoryID].categoryID,
+            name: categories.byID[operation.categoryID].name,
+          };
+
+          return operation;
+        });
+
+      const to = "2012-11-01T12:00:59.000Z";
+
+      const { secretCookie, sessionIDCookie } = await getSessionCookies({
+        password: user02Password,
+        server,
+        username: user02.username,
+      });
+
+      // when
+      const response = await server
+        .get(`/operations?to=${to}`)
         .set("cookie", [secretCookie, sessionIDCookie]);
 
       // then
