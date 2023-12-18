8855a52393e954d424bc26082f7ef4aee8b86778
slacktracer <slacktracer@gmail.com>
2023-12-18 17:56:25 -0300

    Updating "updatedAt" when updating an operation.
    Did I say "update" too much?

--- main/src/domain/modules/operations/update-operation.ts
+++ main/src/domain/modules/operations/update-operation.ts
@@ -6,6 +6,9 @@ export const updateOperation = async ({ operationID, data, userID }) => {
 
   await validateOperationRelatedEntities({ accountID, categoryID, userID });
 
+  data.updatedAt = new Date();
+  data.updatedAtTimezone = data.atTimezone;
+
   return operations.updateOperation({
     operationID,
     data,
