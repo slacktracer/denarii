31330282609fa9205c15f53f1643818ae4896fc4
slacktracer <slacktracer@gmail.com>
2026-04-04 03:47:54 -0300

    Moving to redis in a container.

--- tests/mocks/persistence/connect.js
+++ tests/mocks/persistence/connect.js
@@ -1,16 +1,8 @@
 import { createClient as createRedisClient } from "redis";
-import { RedisMemoryServer } from "redis-memory-server";
 
 import { createDatabaseClient } from "../../../main/src/persistence/create-database-client.js";
 
 export const db = createDatabaseClient();
-
-export const redisServer = new RedisMemoryServer();
-
-const host = await redisServer.getHost();
-const port = await redisServer.getPort();
-
-process.env.REDIS_CONNECTION_STRING = `redis://${host}:${port}`;
 
 export const redisClient = createRedisClient({
   url: process.env.REDIS_CONNECTION_STRING,
--- tests/tools/test.sh
+++ tests/tools/test.sh
@@ -3,5 +3,5 @@ docker compose up -d && \
 prisma migrate dev --name init --schema ../tests/prisma/schema.prisma && \
 prisma generate --schema ../tests/prisma/schema.prisma && \
 vitest --config ./vitest.config.integration.ts && \
-docker stop tests-db-1 && \
+docker stop tests-db-1 tests-redis-1 && \
 rm -fr ./prisma
